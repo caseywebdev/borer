abfec4b9ad8b5db593e7769ac3fd61e0e2124cfa
Casey Foster <c@sey.me>
2021-05-20 10:45:02 -0500

    Handle SIGTERM/SIGINT, 0.1.6

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,13 @@
 
 import commands from './commands/index.js';
 
+const handleSignal = signal => {
+  console.log(`Received ${signal}, exiting immediately...`);
+  process.exit(1);
+};
+
+process.on('SIGINT', handleSignal).on('SIGTERM', handleSignal);
+
 commands.parseAsync(process.argv).catch(er => {
   console.error(er.stack);
   process.exit(1);
